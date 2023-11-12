da1dd6e1750f9b711ac4e2a6d79140b907351010
Sumon <sumon@Sumons-Air.fritz.box>
2023-11-12 19:47:36 +0100

    OK

---
+++ Rank_02/level_2/wdmatch/wdmatch.c
@@ -0,0 +1,36 @@
+#include <unistd.h>
+
+void ft_putstr(char *str)
+{
+    int i = 0;
+    while (str[i])
+    {
+        write (1, &str[i], 1);
+        i++;
+    }
+}
+
+void wdmatch(char *s1, char *s2)
+{
+    int i = 0;
+    int j = 0;
+    
+    while (s1[i] && s2[j])
+    {
+        if (s1[i] == s2[j])
+        {
+            i++;
+        }
+        j++;
+    }
+    if (s1[i] == '\0')
+        ft_putstr(s1);
+}
+
+int main(int ac, char **av)
+{
+    if (ac == 3)
+        wdmatch(av[1], av[2]);
+    write (1, "\n", 1);
+    return (0);
+}
